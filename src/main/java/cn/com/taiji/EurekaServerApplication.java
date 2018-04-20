8a05dcff0fd1944d7cba483d12e1e78e7d99d9ef
unknown <>
2018-04-20 17:38:53 +0800

    第一次提交

---
+++ src/main/java/cn/com/taiji/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package cn.com.taiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//java -jar -Dlogpath=XXX -Dservicename=eurekaSer1 XXX.jar --spring.profiles.active=XXX
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
